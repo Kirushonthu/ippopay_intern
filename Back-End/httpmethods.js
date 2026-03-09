d80d2a2676f3ec2b1440698b4a22f3e236762a9a
Kirushonthu <kishore04072002@gmail.com>
2026-03-09 16:01:58 +0530

    weather_app

--- Back-End/httpmethods.js
+++ Back-End/httpmethods.js
@@ -1,11 +1,14 @@
 console.log("hello")
 
-const url = "https://dummyjson.com/users/1"
+// const url = "https://dummyjson.com/users/1"
 
-const userdata = fetch(url,{
-     method: "POST" ,
-     body: JSON.stringify({ name: "Kishore" }
-)})
+// const userdata = fetch(url,{
+//      method: "POST" ,
+//      body: JSON.stringify({ name: "Kishore" }
+// )})
+fetch("https://api.open-meteo.com/v1/forecast?latitude=13.08&longitude=90.27&current_weather=true")
+  .then(res => res.json())
+  .then(data => console.log(data));
    
  // .then(res => res.json())
     // .then(data => console.log(data.address))
