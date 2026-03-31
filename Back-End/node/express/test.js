076d3786f72d75c630304d5b89e355b508a75538
Kirushonthu <kishore04072002@gmail.com>
2026-03-31 11:47:21 +0530

    test file

---
+++ Back-End/node/express/test.js
@@ -0,0 +1,11 @@
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb+srv://kishore:123%40321@cluster0.g5gdgyc.mongodb.net/kish?retryWrites=true&w=majority")
+// .then(() => console.log("✅ Connected"))
+// .catch(err => console.log("❌ Error:", err.message));
+
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://127.0.0.1:27017/notes")
+// .then(() => console.log("✅ Connected"))
+// .catch(err => console.log(err));
